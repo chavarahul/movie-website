3e99bedbce3a1e2cff03eaa148e504b03ccf432b
chavarahul <chavarahul7@gmail.com>
2024-07-18 21:32:22 +0530

    new website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+import Loader from "./Loader";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const searchMovies = async (title) => {
+    setLoading(true);
+    setError('');
+    try {
+      const response = await fetch(`http://localhost:5000/api/search?q=${title}`);
+      const data = await response.json();
+      setMovies(data);
+    } catch (error) {
+      setError('Failed to fetch movies. Please try again later.');
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    searchMovies('superman');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input 
+          placeholder="Search for movies" 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon} 
+          alt="search"  
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {error && <div className="error">{error}</div>}
+      {loading ? <Loader/> :
+       movies?.length > 0
+       ? (
+         <div className="container">
+           {
+             movies.map((movie, index) => (
+               <MovieCard key={index} movie={movie} />
+             ))
+           }
+         </div>
+       ) : (
+         <div className="empty">
+           <h2>No Movies Found</h2>
+         </div>
+       )
+      }
     </div>
   );
 }
